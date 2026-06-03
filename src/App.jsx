9aa29b81a2912fb1a2e078980a5b912524113512
manohar285 <maheshwarmanohar28@gmail.com>
2026-06-03 17:44:41 +0530

    app js path main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import ListEmployeeComponent from "./assets/Components/ListEmployeeComponent";
 import HeaderComponent from "./assets/Components/HeaderComponent";
 import FooterComponent from "./assets/Components/FooterComponent";
 import Login from "./assets/Components/Login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 import EmployeeComponent from "./assets/Components/EmployeeComponent";
 import Signup from "./assets/Components/Signup";
 function App() {
@@ -12,18 +12,15 @@ function App() {
     <>
       <BrowserRouter>
         <HeaderComponent />
-        <ListEmployeeComponent />
+        
         <Routes>
-          {/*<Route path="/" element={<Login/>}></Route>
+        {/*<Route path="/" element={<Login/>}></Route>
         <Route path="signup" element={<Signup/>}></Route>*/}
-          <Route path="/employees" element={<ListEmployeeComponent />}></Route>
-          <Route path="/employees" element={<ListEmployeeComponent />}></Route>
-          <Route path="/add-employee" element={<EmployeeComponent />}></Route>
+          <Route path="/" element={<ListEmployeeComponent/>}></Route>
+          <Route path="/employees" element={<ListEmployeeComponent/>}></Route>
+          <Route path="/add-employee" element={<EmployeeComponent/>}></Route>
           {/*http://localhost:3000/edit-employee/1*/}
-          <Route
-            path="/edit-employee/:id"
-            element={<EmployeeComponent />}
-          ></Route>
+          <Route path="/edit-employee/:id" element={<EmployeeComponent/>}></Route>
         </Routes>
         <FooterComponent />
       </BrowserRouter>
